fd9ffa871558b685d690206cfc79fbd31f4089c5
luftwaflen <nikitapilkevich@gmail.com>
2023-05-18 17:14:50 +0300

    add Component.Description and midrate db

--- ComputerShopLogic/Dto/ComponentDto.cs
+++ ComputerShopLogic/Dto/ComponentDto.cs
@@ -10,6 +10,7 @@ namespace ComputerShopLogic.Dto
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public decimal Coast { get; set; }
         public List<TagDto> ComponentTags { get; set; }
     }
