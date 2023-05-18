879c218d16eec0f0a24dc63180f9a1b821538a7b
luftwaflen <nikitapilkevich@gmail.com>
2023-05-18 16:26:50 +0300

    create Details modal view

--- ComputerShop.WebApp/Controllers/FindController.cs
+++ ComputerShop.WebApp/Controllers/FindController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ComputerShop.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComputerShop.WebApp.Controllers
 {
     public class FindController : Controller
     {
+        public FindController()
+        {
+        }
         public IActionResult Index()
         {
             return View();
